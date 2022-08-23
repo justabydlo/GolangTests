149323394d74ae7a6fbc5d394a034445cec4925e
Данил Овчинников <dan9904@yandex.ru>
2022-08-23 18:09:40 +0300

    Added log.panic

--- webserver/main.go
+++ webserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -12,5 +13,5 @@ func main() {
 	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello docker")
 	})
-	http.ListenAndServe(":8080", nil)
+	log.Panic(http.ListenAndServe(":8080", nil))
 }
